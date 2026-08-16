ed197c76f959dfb2c4ac29b49a485afd726e144e
Adil <adilgoursa50@gmail.com>
2026-08-16 10:37:16 +0100

    Simplify build configuration and workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.adil.chatapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.adil.chatapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 13
         versionName = "1.1"
     }
